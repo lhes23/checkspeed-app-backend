05ab5806c7bd5c62c78ae12b88f5346dd28d0224
Lester <32542078+lhes23@users.noreply.github.com>
2022-10-10 21:00:07 +0800

    Create a new repository for the Django Backend

---
+++ checkspeed/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from rest_framework.response import Response 
+from rest_framework.decorators import api_view
+import speedtest
+
+@api_view(["GET"])
+def getSpeed(request):
+    test = speedtest.Speedtest(secure=1)
+    down_speed = test.download()/1024/1024
+    up_speed = test.upload()/1024/1024
+    download = round(down_speed,2)
+    upload = round(up_speed,2)
+    return Response({'download':download,'upload':upload})
